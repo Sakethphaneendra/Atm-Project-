2f87b0dcd2e3bbe3f91017090c1aeab63eebfc4b
Saketh Phaneendra Kondisetti <sakethphaneendra@gmail.com>
2024-05-10 11:15:41 +0530

    Version 1 Deployed

---
+++ ProjectATM/src/Project_Version_1/ATMV1.java
@@ -0,0 +1,13 @@
+package Project_Version_1;
+
+public class ATMV1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nWelcome to Bitcoin Machine \nKindly Enter Your Credentials Here \n");
+		UserOperations UO = new UserOperations();
+		
+		UO.User_Validation();
+	}
+
+}
